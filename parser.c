e4ad48103adef91584358709b995a63a2d997f7e
michals96 <37269920+michals96@users.noreply.github.com>
2020-06-15 22:38:31 +0200

    Parser description

--- parser.c
+++ parser.c
@@ -1,5 +1,11 @@
 #include "parser.h"
 
+#define SIZE_OF_TOKEN 64
+#define TOKENS " \t\r\n\a"
+
+/* This function will take command line as input and 
+it will split it into tokens */
+
 char **parser(char *str)
 {
     return 's';
