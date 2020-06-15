7271e5e8956d5f56cca43c882a7433ab3e1030d3
michals96 <37269920+michals96@users.noreply.github.com>
2020-06-15 22:14:59 +0200

    current dir listing

--- main.c
+++ main.c
@@ -1,8 +1,27 @@
 #include<stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+
+typedef struct globals
+{
+    char *directory;
+    char current_directory[1024];
+    
+} globals;
 
 int main()
 {
-    printf("Hello World! \n");
+    globals value;
+
+    while(1)
+    {
+        if(getcwd(value.current_directory, sizeof(value.current_directory)) != NULL)
+            printf("\n%s ", value.current_directory);
+    }
     
     return 0;
 }
