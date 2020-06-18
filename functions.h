4f84432d2cb6bc8495067ee957c4381df1da1184
michals96 <37269920+michals96@users.noreply.github.com>
2020-06-18 23:20:11 +0200

    Move parser to functions

--- functions.h
+++ functions.h
@@ -5,4 +5,10 @@ char **handle_arguments(int type, char **arguments);
 
 void process_executor(int type, char**arguments);
 
+/* This function will take command line as input and 
+it will split it into tokens for example
+Input : Hello world!
+output {"Hello", "world!"} */
+char **parser(char *str);
+
 #endif
